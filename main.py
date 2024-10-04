3f0d063b654b979c581162c916883b27c7c60f7f
Muneeb Ahmed <ahmed4507537@cloud.neduet.edu.pk>
2024-10-04 15:19:37 +0500

    configuing sqlite

--- main.py
+++ main.py
@@ -14,8 +14,20 @@ import pickle
 from pathlib import Path
 
 import streamlit_authenticator as stauth
+import sqlite3
 
 load_dotenv()
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import streamlit as st
+from streamlit import logger
+# import sqlite3
+app_logger = logger.get_logger('SMI_APP')
+app_logger.info(f"sqlite version: {sqlite3.sqlite_version}")
+app_logger.info(f"sys version: {sys.version}")
+
 
 st.set_page_config(page_title="ChatMaster Pro", page_icon="🤖", layout="wide")
 client = OpenAI()
