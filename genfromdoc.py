563b8bb900b8bd2bd17df7b9152e70226de5d444
Muneeb Ahmed <ahmed4507537@cloud.neduet.edu.pk>
2024-10-04 14:43:53 +0500

    init

---
+++ genfromdoc.py
@@ -0,0 +1,46 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based on the following context and your own knowledge:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def generateFromEmbeddings(query_text="", model="gpt-3.5-turbo") -> tuple[str, list[str]]:
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    model = ChatOpenAI(api_key=os.environ["OPENAI_API_KEY"], model="gpt-3.5-turbo")
+    response_text = model.invoke(prompt).content
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    return (response_text, sources)
+
